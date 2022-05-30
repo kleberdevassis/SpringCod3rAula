b2e1d5c0a05695b0666554642e782a333d99dfce
vanes <vanes@LAPTOP-DG4J0EB8>
2022-05-30 19:21:48 -0300

    atualização inicio spring

--- exercicios-SB/src/main/java/br/com/example/exerciciosSB/controllers/primeiroController.java
+++ exercicios-SB/src/main/java/br/com/example/exerciciosSB/controllers/primeiroController.java
@@ -1,12 +1,13 @@
 package br.com.example.exerciciosSB.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class primeiroController {
-	
-	@RequestMapping
+
+	// @RequestMapping(method = RequestMethod.GET, path = "/ola")
+	@GetMapping(path = { "/ola", "/saudacao" })
 	public String Ola() {
 		return "Ola Spring Boot";
 	}
